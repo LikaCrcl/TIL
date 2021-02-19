e80b80fe22a2cf7e4d528762c84860e87e19f021
hyeonmo_ <ahgusdl288@gmail.com>
2021-02-19 16:43:07 +0900

    pc에서작업하기위한커밋

---
+++ work/day_0219/src/com/Day_0219_a.java
@@ -0,0 +1,114 @@
+package com;
+
+import java.util.Scanner;
+
+public class Day_0219_a {
+
+	public static void main(String[] args) {
+		
+		// while문 복습
+//		calculator(2);
+		
+		// 구구단, 변수가 0일때 짝수단, 1일때 홀수단, 2일때 전체 출력
+//		gugudan(1);
+		
+		// do while문 : true false 상관 없이 무조건 1번은 실행함. 실행 후 true라면 계속 실행, false라면 더 이상 실행하지 않음
+//		int count = 0;
+//		do {
+//			System.out.println("do");
+//		}
+//		while (false);
+//		
+//		do {
+//			count++;
+//			System.out.println("do : "+count);
+//			if (count == 9)
+//				break;
+//		}
+//		while (true);
+//		
+//		// 0이하 or 100 이상 입력하면 끝나는 do while문
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("숫자 입력");
+//		
+//		do {
+//			int num = input.nextInt();
+//			System.out.println("test "+num);
+//			if (num <= 0 || num > 99)
+//				break;
+//			System.out.println("재 입력");
+//		}
+//		while (true);
+//		System.out.println("테스트 끝");
+		
+		// innerMax
+//		
+//		int max = 1;
+//		while (max < 10) {
+//			max++;
+//			int innerMax = 10;
+//			while (innerMax > 0) {
+//				if (innerMax < max) {
+//					System.out.print("*");
+//				}
+//				else {
+//					System.out.println(" ");
+//				}
+//				innerMax--;
+//			}
+//		}
+//		
+//		System.out.println();
+		
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//				if (i < j) {
+//					System.out.print("/");
+//				}
+//				else {
+//					System.out.print("*");
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+		// 이등변삼각형 피라미드 만들기
+		for (int i = 1; i < 10; i++){
+			for (int j = 10; j > 0; j--) {
+				if (j < 10) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print("-");
+				}
+			}
+		System.out.println();
+		}
+	}
+	
+	static void calculator(int n) {
+		int count = 1;
+		while (count < 10) {
+			System.out.println(n+"*"+count+"="+n*count);
+			count++;
+		}
+	}
+	
+	static void gugudan (int n) {
+		int count = 1;
+		while (count < 10) {
+			count++;
+			if (count % 2 == n) {
+				calculator(count);
+				System.out.println("true : "+count);
+			}
+			if (n == 2) {
+				calculator(count);
+				System.out.println("true : "+count);
+			}
+			if (count == 9)
+				break;
+		}
+	}
+
+}
---
+++ work/day_0219/src/com/Day_0219_b.java
@@ -0,0 +1,440 @@
+	package com;
+
+	import java.util.Scanner;
+
+	public class Day_0219_b {
+		
+		static int temp;
+		static int a;
+		static int b;
+		static int c;
+		static int d;
+		static int e;
+		
+
+		public static void main(String[] args) {
+			
+			short_test3();
+
+		} // main
+		
+		
+		
+		static void short_change() {
+			
+			if(b < c) {
+				temp = b;
+				b = c;
+				c = temp;
+				if(a < b) {
+					temp = a;
+					a = b;
+					b = temp;
+					if(c < d) {
+						temp = c;
+						c = d;
+						d = temp;
+						if(b < c) {
+							temp = b;
+							b = c;
+							c = temp;
+							if(a < b) {
+								temp = a;
+								a = b;
+								b = temp;
+								if(d < e) {
+									temp = d;
+									d = e;
+									e = temp;
+									if(c < d) {
+										temp = c;
+										c = d;
+										d = temp;
+										if(b < c) {
+											temp = b;
+											b = c;
+											c = temp;
+											if(a < b) {
+												temp = a;
+												a = b;
+												b = temp;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+	   static void short_test3() {
+			
+			a = (int) (Math.random()*46);
+			b = (int) (Math.random()*46);
+			c = (int) (Math.random()*46);
+			d = (int) (Math.random()*46);
+			e = (int) (Math.random()*46);
+			
+			System.out.println("Original random numbers : ");
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println("  ");
+			
+			if(a < b) {
+				temp = a;
+				a = b;
+				b = temp;
+				short_change();
+			}
+			else {
+				short_change();
+			}
+			
+							
+			System.out.println("Numbers in order : ");		
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+		}
+		
+		static void short_ab() {
+			if(a < b) {
+				temp = a;
+				a = b;
+				b = temp;
+			}
+		}
+		
+		static void short_bc() {
+			if(b < c) {
+				temp = b;
+				b = c;
+				c = temp;
+			}
+		}
+		
+		static void short_cd() {
+			if(c < d) {
+				temp = c;
+				c = d;
+				d = temp;
+			}
+		}
+		
+		static void short_de() {
+			if(d < e) {
+				temp = d;
+				d = e;
+				e = temp;
+			}
+		}
+		
+		
+		static void short_test4() {
+			
+			a = (int) (Math.random()*46);
+			b = (int) (Math.random()*46);
+			c = (int) (Math.random()*46);
+			d = (int) (Math.random()*46);
+			e = (int) (Math.random()*46);
+			
+			System.out.println("Original random numbers : ");
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println("  ");
+			
+			if(a < b) {
+				temp = a;
+				a = b;
+				b = temp;
+				short_bc();
+				short_ab();
+				short_cd();
+				short_bc();
+				short_ab();
+				short_de();
+				short_cd();
+				short_bc();
+				short_ab();
+			}
+			else {
+				short_bc();
+				short_ab();
+				short_cd();
+				short_bc();
+				short_ab();
+				short_de();
+				short_cd();
+				short_bc();
+				short_ab();
+			}
+			
+							
+			System.out.println("Numbers in order : ");		
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+		}
+		
+		
+		static void star_grid() {
+//			for (int i = 1; i < 10; i++) {
+//			for (int j = 0; j < i; j++) {
+//				System.out.print("*"+" ");
+//			}
+//			System.out.println("");
+//			
+//		}
+		
+//		for (int i = 10; i > 0; i--) {
+//			for (int j = 0; j < i; j++) {
+//				System.out.print(j + "_");
+//			}
+//			System.out.println(" ");
+//		}
+		
+
+	//	
+//		for(int i=1;i<10;i++)
+//		{
+//			for(int m=1;m<10-i;m++)
+//			{
+//				System.out.print(" ");
+//			}
+//			
+//			for(int j=0;j<i;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+	//	
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 10; j > i; j--) {
+//				System.out.print(" ");
+//			}
+//			for (int x = 10; x>10-i  ; x--) {
+//				System.out.print("*"); 
+//			}
+	//
+//			System.out.println("");
+//		}
+	//	
+	//	
+//		for (int i = 1; i < 10; i++) {
+//			
+//			for (int m = 1; m <10- i; m++) {
+//				System.out.print(" ");
+//			}
+//			for (int j = 0; j < i; j++) {
+//				 System.out.print("*");
+//			}
+//			System.out.println();
+//			   
+//		}
+			
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 9; j > 0; j--) {
+//				if(i <j ) {
+//					System.out.print(" ");
+//				}else {
+//					System.out.print("*");
+//					
+//				} 
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		int max = 1;
+//		while(max < 10) {
+//			max++;
+//			int innerMax = 1;
+//			while(innerMax < max) {
+//				System.out.print("*");
+//				innerMax++;
+//			}
+//			System.out.println();
+//		}
+		
+//		int max=10;
+//		while(max>1) {
+//			max--;
+//			int innerMax = 1;
+//			while(innerMax < 10) {
+//				if(!(innerMax < max))
+//				System.out.print("*");
+//				else
+//				System.out.print(" ");
+//				innerMax++;
+//			}
+//			System.out.println();
+//		}
+		
+//		int max1 =1;
+//		while(max1<10) {
+//			max1++;
+//			int innerMax=10;
+//			while(innerMax > 0) {
+//				if(innerMax<max1)
+//					{
+//					System.out.print("*");
+//					}
+//				else
+//				{
+//					System.out.print(" ");
+//					}
+//				innerMax--;
+//			}
+//			System.out.println();
+//		}
+
+	//	
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 9; j > 0; j--) {
+//				
+//				if(i < j) {
+//					System.out.print(" ");
+//				}else {
+//					System.out.print("*");
+//				}
+//			}
+//			System.out.println("");
+//		}
+		
+		
+		for (int i = 1; i < 10; i++) {
+			
+			for (int j = 0; j < 10-i; j++) {
+				System.out.print("a");
+			}
+			for (int j = 1; j < i*2; j++) {
+				
+				System.out.print("*");
+			}
+			for (int j = 0; j < 10-i; j++) {
+				System.out.print("a");
+			}
+			
+			System.out.println("");
+		}
+		
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 0; j < 10-i; j++) {
+//				System.out.print("*");
+//				
+//			}
+//			for (int j = 10; j> 10-i; j--) {
+//				System.out.print("/");
+//			}
+//			
+//			for (int j = 0; j < i ; j++ ) {
+//				
+//				System.out.print("0");
+//			}
+	//
+//			for (int j = 10; j > i; j--) {
+//				System.out.print("1");
+//				
+//			}
+//			System.out.println();
+//		}
+
+
+		
+			
+		}
+
+		static void while_do_input() {
+			
+			// 0 이하이거나 100 이상일 때 입력하면 do while 나가기 or
+					Scanner input = new Scanner(System.in);
+					System.out.println("아무 숫자나 입력하세요");
+					do {
+						int num = input.nextInt();
+						System.out.println("test"+num);
+						if(num <= 0 || num >= 100)
+							break;
+						System.out.println("또 입력하세요");
+					}while(true);
+					System.out.println("do을 나왔어요");
+			
+		}
+		
+		static void while_test3() {
+			int count = 0;
+
+			do {
+				count++;
+				System.out.println("do");
+				if(count == 9)
+					break;	
+			}while(true);
+
+			count = 0;
+			do {
+
+				count++;
+				System.out.println("do :" + count);
+				//			if(count == 9)
+				//				break;	
+			}while(count < 9);
+
+		
+		}
+		
+		static void gugudan(int n) {
+			int count = 1;
+			while(true) {
+				count++;
+				if(n == 2) {
+					caculator(count);
+				}
+				else if(count % 2 == n) {
+					caculator(count);
+				}
+				
+				if(count == 9)
+					break;
+			}	
+		}
+		
+		static void  caculator(int n) {
+			int count = 1;
+			while(count < 10) {
+				System.out.println(n + "*"+count+"="+ n* count);
+				count++;
+			}
+		}
+			
+		static void while_test() {
+			int count = 2;
+			while(count < 10) {
+				int j= 1;
+				while(j < 10) {
+					System.out.println(count);
+					j++;
+					
+				}
+				count++;
+				System.out.println("--------------"+count);
+				System.out.println("--------------");
+				
+			}
+		}
+
+	}
+
