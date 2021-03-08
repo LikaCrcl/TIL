247bb653660cd679bf9a229a2d0e1b1a0f573993
hyeonmo_ <ahgusdl288@naver.com>
2021-03-08 16:26:10 +0900

    중간커밋

---
+++ work/day_0308/src/com/Day_0308_a.java
@@ -0,0 +1,48 @@
+package com;
+
+public class Day_0308_a {
+
+	public static void main(String[] args) {
+		
+		// int 2차배열을 String 2차배열에 대입하여 표현해보기 예제
+		int[][] gugudan = new int[9][9];
+		String[][] gu_string = new String[9][9];
+		
+		for (int i = 0; i < gugudan.length; i++) {
+			for (int j = 0; j < gugudan[0].length; j++) {
+				gugudan[i][j] = (i+1)*(j+1);
+//				System.out.println(gugudan[i][j]);
+			}
+//			System.out.println();
+		}
+				
+		for (int i = 0; i < gu_string.length; i++) {
+			for (int j = 0; j < gu_string[0].length; j++) {
+				gu_string[i][j] = (i+1)+"*"+(j+1)+"="+gugudan[i][j];
+//				System.out.println(gu_string[i][j]);
+			}
+		}
+		
+		String[] a = new String[gu_string.length*gu_string[0].length];
+		
+		int count = 0;
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = "";
+		}
+		
+		for (int i = 0; i < gu_string.length; i++) {
+			for (int j = 0; j < gu_string[0].length; j++) {
+				a[count++] = gu_string[i][j];
+			}
+		}
+		
+		count = 0;
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
