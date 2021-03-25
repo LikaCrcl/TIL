d66f43004f215ffb8ddb60a6cd67340bcd17ac62
hyeonmo_ <ahgusdl288@naver.com>
2021-03-25 21:50:45 +0900

    interface, abstract, swing 복습

--- work/day_0325/src/com/B.java
+++ work/day_0325/src/com/B.java
@@ -1,18 +1,67 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class B extends A {
 
 	@Override
 	public void i() {
 		
+		File file = new File("C:\\Users\\ahgus\\Desktop\\JAVA_PROJECT\\work\\day_0325\\text\\t.txt");
+		String name = "hello";
+		StringBuilder sb = new StringBuilder("helloworld");
+		StringBuffer sbb = new StringBuffer("helloworld_buffer");
+		File file2 = new File("C:\\Users\\ahgus\\Desktop\\JAVA_PROJECT\\work\\day_0325\\text\\s.txt");
 		
+		byte[] bytes = sb.toString().getBytes();
+		
+		try {
+			
+			FileWriter writer = new FileWriter(file);
+			BufferedWriter b_writer = new BufferedWriter(writer);
+			b_writer.write(sbb.toString());
+			System.out.println(sbb);
+			b_writer.close();
+			
+			FileOutputStream stream = new FileOutputStream(file2);
+			stream.write(bytes);
+			stream.close();
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			
+		} finally {
+			
+			System.out.println("매번 출력");
+			
+		}
 		
 	}
 
 	@Override
 	void a() {
 		
+		File file = new File("C:\\Users\\ahgus\\Desktop\\JAVA_PROJECT\\work\\day_0325\\text\\t.txt");
 		
+		try {
+			
+			FileReader read = new FileReader(file);
+			BufferedReader b_read = new BufferedReader(read);
+			System.out.println("출력이 됩니다 : "+b_read.readLine());
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			
+		}
 		
 	}
 
--- work/day_0325/src/com/Day_0325.java
+++ work/day_0325/src/com/Day_0325.java
@@ -5,8 +5,9 @@ public class Day_0325 {
 	public static void main(String[] args) {
 		
 		B b = new B();
-		b.a();
+		
 		b.i();
+		b.a();
 
 	}
 
